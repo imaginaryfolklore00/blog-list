c01d5315e5367ca0b3dc9e8dd79f6941c1fd0aef
Nazeem <nazeem.s2k@gmail.com>
2021-08-28 17:11:06 +0500

    added config.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -14,7 +14,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(result => {
     logger.info('connected to MongoDB')

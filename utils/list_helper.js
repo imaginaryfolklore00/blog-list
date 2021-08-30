35a34020b430c878eec709a93f5bcb717f0a6303
Nazeem <nazeem.s2k@gmail.com>
2021-08-30 16:48:15 +0500

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => sum + item
+    
+    return blogs 
+    ? blogs.map((blog) => blog.likes).reduce(reducer, 0) 
+    : 0
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }

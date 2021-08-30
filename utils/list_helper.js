0c36b1d1c9b34d0daf1368908fcbc2ba9a10c27b
Nazeem <nazeem.s2k@gmail.com>
2021-08-30 17:41:42 +0500

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,6 @@
+const blogsRouter = require("../controllers/blogs")
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
     return 1
 }
@@ -10,7 +13,21 @@ const totalLikes = (blogs) => {
     : 0
 }
 
+const favoriteBlog = (blogs) => {
+    const mostLikedBlogs = blogs.filter((blog) => blog.likes === Math.max(...blogs.map((blog) => blog.likes)))
+    const firstFavBlog = mostLikedBlogs[0]
+    
+    const returnedBlog = {
+        title: firstFavBlog.title,
+        author: firstFavBlog.author,
+        likes: firstFavBlog.likes
+    }
+
+    return returnedBlog
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }

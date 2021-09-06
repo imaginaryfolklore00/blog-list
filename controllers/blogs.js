1b3c4b943f689e3eaf034291dd93c2f2f928e311
Nazeem <nazeem.s2k@gmail.com>
2021-09-06 12:30:23 +0500

    4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -19,5 +19,24 @@ blogsRouter.post('/', async (req, res) => {
   const savedBlog = await blog.save()
   res.json(savedBlog.toJSON())
 })
-    
+
+blogsRouter.delete('/:id', async(req, res) => {
+  await Blog.findByIdAndRemove(req.params.id)
+  res.status(204).end()
+})
+
+blogsRouter.put('/:id', async(req, res) => {
+  const body = req.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+  res.json(updatedBlog.toJSON())
+})
+
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -84,6 +84,49 @@ test('if properties "url" and "title" are missing when posting, respond with an 
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 })
 
+test('a blog is deleted correctly', async() => {
+    const blogsAtStart = await helper.blogsInDB()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+    
+    const blogsAtEnd = await helper.blogsInDB()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).not.toContain(blogToDelete.title)
+})
+
+test('a blog is updated correctly', async() => {
+    const updatedParameters = {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 8
+    }
+    const blogsAtStart = await helper.blogsInDB()
+    const blogToUpdate = blogsAtStart[0]
+
+    await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(updatedParameters)
+        .expect(200)
+    
+    const blogsAtEnd = await helper.blogsInDB()
+    const updatedBlog = blogsAtEnd[0]
+
+    const comparedParameters = {
+        title: updatedBlog.title,
+        author: updatedBlog.author,
+        url: updatedBlog.url,
+        likes: updatedBlog.likes
+    }
+
+    expect(comparedParameters).toEqual(updatedParameters)
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })

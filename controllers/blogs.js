f02dc0e6d50f81d48c52dfccc2339a701680ed7e
Nazeem <nazeem.s2k@gmail.com>
2021-09-08 17:44:16 +0500

    4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,35 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user')
+
+
   res.json(blogs.map(blog => blog.toJSON()))
 })
   
 blogsRouter.post('/', async (req, res) => {
   const body = req.body
 
+  const users = await User.find({})
+  const user = users[0]
+
+  console.log(user)
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   res.json(savedBlog.toJSON())
 })
 
